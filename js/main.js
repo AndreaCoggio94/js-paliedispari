c2aff805dce52062c5a13ca14cd38d51447eeeb7
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-07-27 15:10:45 +0200

    added second part for "pari e dispari"

--- js/main.js
+++ js/main.js
@@ -12,3 +12,19 @@ function randomNumber(min, max) {
   let number = Math.floor(Math.random() * (max - min)) + min;
   return number;
 }
+
+const winnerNumber = userNumber + webNumber;
+
+console.log(winnerNumber);
+
+function evenNumber(sum) {
+  if (sum % 2 == 0) {
+    console.log("pari");
+    return true;
+  } else {
+    console.log("dispari");
+    return false;
+  }
+}
+
+const result = evenNumber(winnerNumber);
